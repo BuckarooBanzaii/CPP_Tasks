700abcd43f533cb638e0566af4a153161e75882c
BuckarooBanzaii <111050150+BuckarooBanzaii@users.noreply.github.com>
2023-07-14 17:44:11 +0300

    Add 7 task

---
+++ Simple_Calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	double x, s;
+	char op;
+	cout << " >> ";
+	cin >> x;
+	s = x;
+	while (true) {
+		cout << ">> ";
+		cin >> op;
+		if (op == '=') {
+			cout << "---> " << s << endl;
+			exit(0);
+		}
+		cout << ">>";
+		cin >> x;
+		switch (op) {
+			case '+' :
+				s += x;
+				break;
+			case '-': 
+				s *= x;
+				break;
+			case '/':
+				s /= x;
+		}
+	}
+	return 0;
+}
