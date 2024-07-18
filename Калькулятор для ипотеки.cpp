38b4e8355fe12504655ec065501657af0950dc48
BuckarooBanzaii <111050150+BuckarooBanzaii@users.noreply.github.com>
2024-07-18 20:08:24 +0300

    Add files via upload

---
+++ Калькулятор для ипотеки.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double loanAmount; // Сумма займа
+    double annualInterestRate; // Годовая процентная ставка
+    int loanTerm; // Срок займа в месяцах
+
+    cout << "Введите сумму займа: $";
+    cin >> loanAmount;
+
+    cout << "Введите годовую процентную ставку (например, 5% вводится как 0.05): ";
+    cin >> annualInterestRate;
+
+    cout << "Введите срок займа в месяцах: ";
+    cin >> loanTerm;
+
+    double monthlyInterestRate = annualInterestRate / 12; // Месячная процентная ставка
+    double monthlyPayment = (loanAmount * monthlyInterestRate) / (1 - pow(1 + monthlyInterestRate, -loanTerm));
+
+    cout << "Месячный платеж составит: $" << monthlyPayment << endl;
+
+    return 0;
+}
